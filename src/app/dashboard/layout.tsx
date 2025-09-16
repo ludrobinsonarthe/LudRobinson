bcdf2f45ba02f43a08331b905daf64debd48967a
Sfb picture pro officiel <semfransbbourangon@gmail.com>
2025-09-16 09:29:04 +0000

    affiche les sur le coté gauches avec les autres onglet

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -109,25 +109,20 @@ function MainSidebar() {
             </SidebarMenuItem>
           ))}
 
-          {user?.role === 'admin' && (
-            <SidebarGroup>
-                <SidebarGroupLabel>Gestion</SidebarGroupLabel>
-                {adminManagementItems.map((item) => (
-                    <SidebarMenuItem key={item.href}>
-                        <SidebarMenuButton
-                            asChild
-                            isActive={pathname === item.href}
-                            tooltip={item.label}
-                        >
-                            <Link href={item.href}>
-                            <item.icon />
-                            <span>{item.label}</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-            </SidebarGroup>
-          )}
+          {user?.role === 'admin' && adminManagementItems.map((item) => (
+            <SidebarMenuItem key={item.href}>
+                <SidebarMenuButton
+                    asChild
+                    isActive={pathname === item.href}
+                    tooltip={item.label}
+                >
+                    <Link href={item.href}>
+                    <item.icon />
+                    <span>{item.label}</span>
+                    </Link>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
 
           <SidebarMenuItem>
             <SidebarMenuButton
