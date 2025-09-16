62e4a404b6a125e6f3780817c8bf81639f7828f7
Sfb picture pro officiel <semfransbbourangon@gmail.com>
2025-09-16 10:57:58 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAimyg0l1mQwxvIWSnoMGMcs3O9_CICMT0",
+  authDomain: "studio-5723170349-4a6fe.firebaseapp.com",
+  projectId: "studio-5723170349-4a6fe",
+  storageBucket: "studio-5723170349-4a6fe.appspot.com",
+  messagingSenderId: "830599496194",
+  appId: "1:830599496194:web:447e25cfefcc7ac68a5d1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
