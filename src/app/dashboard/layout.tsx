9409f2f67acaa3a168fb2bec581ec3599489aaa9
Sfb picture pro officiel <semfransbbourangon@gmail.com>
2025-09-17 12:29:59 +0000

    Try fixing this error: `Runtime Error: Error: useRouter is not defined.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link";
-import { usePathname, redirect } from "next/navigation";
+import { usePathname, redirect, useRouter } from "next/navigation";
 import {
   SidebarProvider,
   Sidebar,
