a0032d5360789083181cfcf191847c8c0f73d77a
Robinson Arthe <robinsonarthe@gmail.com>
2025-09-24 21:57:47 +0000

    Try fixing this error: `Runtime Error: Error: useFieldArray is not defin

--- src/components/attendance-dialog.tsx
+++ src/components/attendance-dialog.tsx
@@ -1,7 +1,6 @@
-
 "use client";
 
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Button, buttonVariants } from "@/components/ui/button";
