c84707a3d32b069c6602ed055006fed63838da97
Robinson Arthe <robinsonarthe@gmail.com>
2025-09-27 14:51:56 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
+import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
