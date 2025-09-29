5a12fd55c045b896e6f171cb178bd74873cb4bb3
Robinson Arthe <robinsonarthe@gmail.com>
2025-09-29 07:02:52 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@genkit-ai/core'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.weserv.nl',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
